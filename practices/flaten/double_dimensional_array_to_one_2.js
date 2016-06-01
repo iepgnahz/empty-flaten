71e6808e207cc056435fd728abb801cb62858e64
zhangpei <13488233237@163.com>
2016-06-01 21:24:55 +0800

    finish problem

--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -1,8 +1,34 @@
 'use strict';
 
 function double_to_one(collection) {
+  var arr = [];
+  var num = 0;
+  for(var i = 0; i < collection.length; i++) {
+    if(collection[i].length){
+      for(var j = 0; j < collection[i].length; j++) {
+        num = collection[i][j];
+        if(scan(arr,num,arr.length)) {
+          arr.push(num);
+        }
+      }
+    } else {
+      num = collection[i];
+      if(scan(arr,num,arr.length)) {
+        arr.push(num);
+      }
+    }
+  }
+  return arr;
+}
 
-  //在这里写入代码
+function scan(arr,num,index) {
+  var flag = true;
+  for(var i = 0; i < index; i++) {
+    if(num == arr[i]){
+      flag = false;
+    }
+  }
+  return flag;
 }
 
 module.exports = double_to_one;
